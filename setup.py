8cdb3a860e93670a21304c6ece20324a2fc5518a
Balachandran Sivakumar <balachandran@balachandran.org>
2015-10-06 09:59:49 +0530

    Fixed typos in setup.py
    There were a few Copy+Paste errors causing sytax errors when running
    setup.py. Fixed the errors.

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ setup (
     version = '0.1',
     url='http://github.com//sandman/',
     license='Apache Software License v2',
-    author="Balachandran S <balachandran@balachandran.org>, Abhisek Kumar Rout <abhisek.rout93@gmail.com>"
+    author="Balachandran S <balachandran@balachandran.org>, Abhisek Kumar Rout <abhisek.rout93@gmail.com>",
     install_requires=['Flask>=0.10.1',
                       'Flask-SQLAlchemy>=2.0',
                       'SQLAlchemy>=1.0.8',
                       'pytest'
                   ],
-    description='A simple, usable bug tracking system'
-    long_description=open("README.adoc").read()
+    description='A simple, usable bug tracking system',
+    long_description=open("README.adoc").read(),
     packages=['bugvault'],
     include_package_data=True,
     platforms='any',
